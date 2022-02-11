b6397f1702034720d5bf2080738a39c81817034f
dom <dompec07@gmail.com>
2022-02-11 18:08:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import os
 
-# Define colours
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 # Whitespace
 print("")
 
-site = input(bcolors.BOLD + "Enter a website to steal:\n" + bcolors.ENDC + ">> ")
+site = input("\033[1mEnter a website to steal:\n\033[0m >> ")
 
 # wget -r --user-agent=Mozilla --page-requisites --convert-links -e robots=off https://cdn.openttd.org
 cmd = "wget -r --user-agent=Mozilla --page-requisites --convert-links -e robots=off " + site
-
-print("\n"+cmd)
 
 # Run the command
 os.system(cmd)
