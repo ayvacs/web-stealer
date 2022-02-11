61cf2853369865c75eb4a5eeb1d1fd998197d3b4
dom <dompec07@gmail.com>
2022-02-11 18:13:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import os
 print("")
 
 site = input("\033[1mEnter a website to steal:\n\033[0m >> ")
-
-# wget -r --user-agent=Mozilla --page-requisites --convert-links -e robots=off https://cdn.openttd.org
 cmd = "wget -r --user-agent=Mozilla --page-requisites --convert-links -e robots=off " + site
 
 # Run the command
