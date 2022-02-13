ba87248c6c52ba6b61ac2c9911d9a7b627c30341
dom <dompec07@gmail.com>
2022-02-12 23:16:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 print("")
 
 site = input("\033[1mEnter a website to steal:\n\033[0m >> ")
-cmd = "wget -r --user-agent=Mozilla --page-requisites --convert-links -e robots=off " + site
+cmd = "wget -nc -r --user-agent=Mozilla --page-requisites -e robots=off " + site
 
 # Run the command
 print("")
